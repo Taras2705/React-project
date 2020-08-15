b6969584edbf6b870717a97f69aed53550b48645
Taras Kusiy <taraskusiy@gmail.com>
2020-08-15 16:29:18 +0300

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header=()=>{
+  return(
+      <header className="header">
+          <img className="logo" src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3f/Gold%27s_Gym_logo.svg/1200px-Gold%27s_Gym_logo.svg.png" alt=""/>
+      </header>
+  )
+
+};
+export default Header;
---
+++ src/Components/Profile.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+    return (<div className="content">
+            <img className="content-img"
+                 src="https://bk.asia-city.com/sites/default/files/u143592/webp.net-compress-image_38_0.jpg"/>
+            <div>
+                ava+discription
+            </div>
+            <div>my posts
+            </div>
+            <div>new post</div>
+            <div>post 1
+            </div>
+            <div>post 2</div>
+        </div>
+
+    )
+};
+export default Profile;
