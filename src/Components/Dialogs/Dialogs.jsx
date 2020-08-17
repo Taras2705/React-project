c01024b6633d714d38de9bb3b19faf0d1fe842c8
Taras Kusiy <taraskusiy@gmail.com>
2020-08-17 20:11:40 +0300

    Dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import s from './Dialogs.module.css';
-const Dialogs=(props)=>{
-    return(
-        <div className={s.dialog}>
-            Dialogs
+
+const Dialogs = (props) => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogItem}>
+                <div className={`${s.dialog} ${s.active}`}>Ivan</div>
+                <div className={s.dialog}>Petro</div>
+                <div className={s.dialog}>Oleksandr</div>
+                <div className={s.dialog}>Taras</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi hi</div>
+                <div className={s.message}>Hi how are you?</div>
+                <div className={s.message}>Hi.Nice to meet you</div>
+                <div>
+                    <textarea className={s.messageTo}></textarea>
+                </div>
+                <div className={s.buttons}>
+                    <button className={s.sent}>Sent message</button>
+                    <button className={s.sent}>Attache folder</button>
+                </div>
+            </div>
+
         </div>
     )
 };
