bde172ab6d015aaacde047929d2cf20a133c075e
Taras Kusiy <taraskusiy@gmail.com>
2020-08-18 17:41:21 +0300

    rerender components DialogItem and MessageItem

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,21 +1,9 @@
 import React from "react";
 import s from './Dialogs.module.css';
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import MessageItem from "./MessageItem/MessageItem";
 
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id;
-    return (
-        <div className={`${s.dialog} ${s.active}`}>
-            <NavLink to={path}>{props.name}
-            </NavLink>
-        </div>
-    )
-};
-const MessageItem = (props) => {
-    return (
-        <div className={s.message}>{props.message}</div>
-    )
-};
+
 
 const Dialogs = (props) => {
     let dialogs = [
